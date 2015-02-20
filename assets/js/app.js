86153f302f03caa0e05ff3110aabe826ff9d0de4
shakaman <mbenadon@shakaman.com>
2015-02-20 09:09:06 +0100

    Debug javascript on ffx

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
   var timerMask = window.setTimeout(showMask, 2000);
 
   function showMask() {
     var random = getRandomArbitrary(1, 16),
         span = document.querySelector('h1 span'),
-        duration = 0.125 * random;
+        duration = 0.125 * random + 's',
+        position = -90 * random + 'px';
 
     span.style.transitionDuration = duration;
-    span.style.backgroundPositionY = -90 * random + 'px';
+    span.style.backgroundPosition = '0 ' + position;
   }
 
   function getRandomArbitrary(min, max) {
     return Math.round(Math.random() * (max - min) + min);
   }
-});
+}
